f2fa0254a85af79d4f96940ec133f73d73c97159
Jose-escapeunreal controls <34663216+joz999@users.noreply.github.com>
2020-03-16 12:11:15 +0530

    Create car.js

---
+++ car.js
@@ -0,0 +1,14 @@
+var car = {
+    
+    make: 'VW',
+    type: 'Polo',
+    color: 'blue',
+    seats: ['seat1','seat2','seat3','seat4'],
+    carturnon: false,
+    turnon: function(){
+        
+        this.carturnon = true;
+        
+    }
+};
+
